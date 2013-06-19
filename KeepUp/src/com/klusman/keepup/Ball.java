6a89911a2b96bcf02ee17a5069506e85b51e1ef0
Shae <shae@fullsail.com>
2013-06-18 19:15:17 -0700

    trying to reformat screen size

---
+++ KeepUp/src/com/klusman/keepup/Ball.java
@@ -0,0 +1,142 @@
+package com.klusman.keepup;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
+
+public class Ball {
+	private static String TAG = "KeepUp";
+	public Texture ballTx;
+	public String textureAddress;
+	private Sprite ballSprite;
+	float sizeX;
+	float sizeY;
+	float xSpeed;
+	float ySpeed;
+	float PositionX;  
+	float PositionY;
+	int rotationSpeed;
+	boolean rotationDirection;
+	boolean collision;
+	
+	
+	public Ball (){
+		
+		
+		textureAddress = "data/grnBall.png";
+		sizeX = .09f;
+		sizeY = .09f;
+		xSpeed = 0.005f;
+		ySpeed = 0.005f;;
+		PositionX = -0.49f;  // change this one
+		//Gdx.app.log(TAG, "xPos:" + PositionX);
+		PositionY = -0.22f;
+		rotationSpeed = 5;
+		rotationDirection = true;
+		collision = false;
+		
+		ballTx = new Texture(Gdx.files.internal(textureAddress));
+		ballTx.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			
+		TextureRegion ballRegion = new TextureRegion(ballTx, 0, 0, ballTx.getWidth(), ballTx.getHeight());
+		ballSprite = new Sprite(ballRegion);
+		ballSprite.rotate(90);
+		ballSprite.setSize(sizeX, sizeY);
+		ballSprite.setOrigin(ballSprite.getWidth()/2, ballSprite.getHeight()/2);
+		ballSprite.setPosition(PositionX, PositionY - ballSprite.getHeight()/2);
+	}
+	
+////	
+	public float getSizeX(){
+		return sizeX;
+	}
+	
+	public float getSizeY(){
+		return sizeY;
+	}
+	
+	public void setSizeXY(float floatSizeX, float floatSizeY){
+		sizeX = floatSizeX;
+		sizeY = floatSizeY;
+		ballSprite.setSize(sizeX, sizeY);
+		ballSprite.setOrigin(ballSprite.getWidth()/2, ballSprite.getHeight()/2);
+	}
+	
+////	
+	public float getXSpeed(){
+		return xSpeed;
+	}
+	
+	public float getYSpeed(){
+		return ySpeed;
+	}
+	
+	public void setXSpeed(float floatXSpeed){
+		xSpeed = floatXSpeed;
+	}
+	
+	public void setYSpeed(float floatYSpeed){
+		ySpeed = floatYSpeed;
+	}
+		
+////
+	public int getRotationSpeed(){
+		return rotationSpeed;
+	}
+	
+	public void setRotationSpeed(int rotSpeed){
+		rotationSpeed = rotSpeed;
+	}
+
+////	
+	public boolean getRotation(){
+		return rotationDirection;
+	}
+	
+	public void setRotation(boolean boolRotationDirection){
+		rotationDirection = boolRotationDirection;
+	}
+	
+////	
+	public boolean getCollision(){
+		return collision;
+	}
+	
+	public void setCollision(boolean boolCollision){
+		collision = boolCollision;
+	}
+	
+////	
+	public float getXPosition(){
+		return PositionX;
+	}
+	
+	public void setXPosition(float xPos){
+		PositionX = xPos;
+	}
+	
+////
+	public float getYPosition(){
+		return PositionY;
+	}
+	
+	public void setYPosition(float yPos){
+		PositionY = yPos;
+	}
+	
+////
+	public Sprite getBallSprite(){
+		return ballSprite;
+	}
+
+	public void draw(SpriteBatch batch) {
+		ballSprite.draw(batch);
+	}
+	
+	
+
+}
--- KeepUp/src/com/klusman/keepup/MainKeepUp.java
+++ KeepUp/src/com/klusman/keepup/MainKeepUp.java
@@ -11,13 +11,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.utils.Array;
 
 
 
@@ -25,80 +24,69 @@ public class MainKeepUp implements ApplicationListener, InputProcessor {
 	private static String TAG = "KeepUp";
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-
+	int Level;
+	double randNumYLoc;
+	double randNumSize;
+	double randNumSpeed;
 
 	private Texture bgTx;
 	private Sprite bg;
 	
-	private Texture grnBallTx;
-	private Sprite ball;
-	
-	private Texture grnBallTx2;
-	private Sprite ball2;
-
 	private Texture kidTx;
-	private Sprite kid;
+	public static Sprite kid;
 	
-	float w;
-	float h;
+	private Texture pauseTx;
+	public static Sprite pause;
+	
+	float x;
+	float y;
+	float screenRatio;
 	float time;
-	
-	int rotateSpeed;
-	float moveSpeed;
-	float YmoveSpeed;
-	Boolean SpinPos;
-	
-	int rotateSpeed2;
-	float moveSpeed2;
-	float YmoveSpeed2;
-	Boolean SpinPos2;
 	
 	public static Sound metalDing;
 	public static Sound buzzer;
 	public static Sound bounce;
 
-	private BitmapFont font; 
+	//private BitmapFont font; 
 	
 	boolean kidMove;
 	public static Music bgMusic;
+	boolean ballCollision;
+	public Array<Ball> Balls;
 
-
+	int screenXRefactor;
+	int screenYRefactor;
 	
 	@Override
 	public void create() {
-		// TWEEEN CRAP
-		//Tween.registerAccessor(man.class, new manAccessor());
-		//TweenManager manager = new TweenManager();
+		x = Gdx.graphics.getWidth();
+		y = Gdx.graphics.getHeight();
+		screenRatio = x/y;
+		screenXRefactor = 1000;
+		screenYRefactor = (int) (screenRatio * screenXRefactor);
+		Gdx.input.setInputProcessor(this);
+		//camera = new OrthographicCamera(1, screenRatio);
+		camera = new OrthographicCamera(screenXRefactor, screenYRefactor);
+		Gdx.app.log(TAG, "SCREEN RATIO Y = " + screenYRefactor);
+		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("audio/Ttimes.mp3"));	
+		bgMusic.setLooping(false);  // Didn't want to kill u with this bad loop
+		bgMusic.setVolume(0.03f);
+		bgMusic.play();
 		
-		w = Gdx.graphics.getWidth();
-		h = Gdx.graphics.getHeight();
-		SpinPos = true;
-		SpinPos2 = true;
+		Balls = new Array<Ball>();
+		Level = 4;  // CURRENT GAME LEVEL
+		levelBallSet(Level);
+
 		kidMove = false;
 		
-		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("audio/Ttimes.mp3"));
 		
-	    bgMusic.setLooping(false);  // Didn't want to kill u with this bad loop
-	    bgMusic.setVolume(0.05f);
-	    bgMusic.play();
-		
-		font = new BitmapFont(); 
-		font.setColor(0.0f, 0.0f, 1.0f, 1.0f); // tint font blue
+		//font = new BitmapFont(); 
+		//font.setColor(0.0f, 0.0f, 1.0f, 1.0f); // tint font blue
 
 		time = Gdx.graphics.getDeltaTime();
 		
-		///  Ball Speeds  ////
-		rotateSpeed = 5;
-		moveSpeed = .005f;
-		YmoveSpeed = .005f;
-		
-		rotateSpeed2 = 8;
-		moveSpeed2 = .008f;
-		YmoveSpeed2 = .005f;
-		
+
 		////  Camera, touch and batch  //////
-		Gdx.input.setInputProcessor(this);
-		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
 		
 		/// AUDIO  ///
@@ -112,55 +100,114 @@ public class MainKeepUp implements ApplicationListener, InputProcessor {
 		bgTx.setFilter(TextureFilter.Linear, TextureFilter.Linear);	
 		TextureRegion bgRegion = new TextureRegion(bgTx, 0, 0, bgTx.getWidth(), bgTx.getHeight() - 90);
 		bg = new Sprite(bgRegion);
-		bg.setSize(h/w, 1);
+		//bg.setSize(screenXRefactor, screenYRefactor);
+		bg.setSize(screenXRefactor, screenXRefactor);
 		bg.rotate(90);
 		bg.setOrigin(bg.getWidth()/2, bg.getHeight()/2);
 		bg.setPosition(0 - bg.getWidth()/2, 0f - bg.getHeight()/2);
 		
-		//// BALL
-		grnBallTx = new Texture(Gdx.files.internal("data/grnBall.png"));
-		grnBallTx.setFilter(TextureFilter.Linear, TextureFilter.Linear);	
-		TextureRegion ballRegion = new TextureRegion(grnBallTx, 0, 0, grnBallTx.getWidth(), grnBallTx.getHeight());
-		ball = new Sprite(ballRegion);
-		ball.setSize(.09f, .09f);
-		ball.setOrigin(ball.getWidth()/2, ball.getHeight()/2);
-		ball.setPosition(-.49f, -0.22f - ball.getHeight()/2);
-
-		//// BALL2
-		grnBallTx2 = new Texture(Gdx.files.internal("data/grnBall.png"));
-		grnBallTx2.setFilter(TextureFilter.Linear, TextureFilter.Linear);	
-		TextureRegion ballRegion2 = new TextureRegion(grnBallTx2, 0, 0, grnBallTx2.getWidth(), grnBallTx2.getHeight());
-		ball2 = new Sprite(ballRegion2);
-		ball2.setSize(.06f, .06f);
-		ball2.setOrigin(ball2.getWidth()/2, ball2.getHeight()/2);
-		ball2.setPosition(-.49f, -0.1f - ball2.getHeight()/2);
 		
 		//// KID
-		
 		kidTx = new Texture(Gdx.files.internal("data/kid.png"));
 		kidTx.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		TextureRegion kidRegion = new TextureRegion(kidTx, 0, 0, kidTx.getWidth(), kidTx.getHeight());
 		kid = new Sprite(kidRegion);
-		kid.setSize(.07f, .09f);
+		kid.setSize(.12f * 1000, .12f * 1000);
 		kid.rotate(90);
 		kid.setOrigin(kid.getWidth()/2, kid.getHeight()/2);
-		kid.setPosition(0 , 0);	
+		kid.setPosition(0.3f  * 1000, 0 - kid.getWidth()/2 );	
 		
+		
+	//// Pause
+			pauseTx = new Texture(Gdx.files.internal("data/redBar.png"));
+			pauseTx.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+			TextureRegion pauseRegion = new TextureRegion(pauseTx, 0, 0, pauseTx.getWidth(), pauseTx.getHeight());
+			pause = new Sprite(pauseRegion);
+			pause.setSize(.09f * 1000, .05f * 1000);
+			//pause.rotate(90);
+			pause.setOrigin(pause.getWidth()/2, pause.getHeight()/2);
+			
+			//pause.setOrigin(pause.getHeight()/2, pause.getWidth()/2);
+			pause.setPosition(-0.5f * 1000, 0);	
+
 
 	} // END CREATE
 
 
+	public void makeNewBall(){
+		Ball ball = new Ball();
+		float yFloat = 0;
+		
+		//// SIZE
+		randNumSize = Math.random();
+		double rand = ((randNumSize * 100) + 1);
+		double rand2 = rand;
+		if (rand <= 40){
+			rand2 = rand + 41; // Makes a number greater than 41
+		} if (rand >= 90){
+			rand2 = rand - ((90 - rand ) * 2);  // makes a number less than 90
+		}
+		float r = (float) (rand2 / 1000);
+		Gdx.app.log(TAG, "Random Size: " + r);
+		
+		ball.setSizeXY(r, r);
+		Gdx.app.log(TAG, "ball X:" + ball.getSizeX());  
+		Gdx.app.log(TAG, "ball Y:" + ball.getSizeY()); 
+		
+		
+		//// SPEED
+		randNumSpeed = Math.random();
+		double sp = ((randNumSize * 5) + 5);
+		int spInt = (int)sp;
+		Gdx.app.log(TAG, "Speed random " + spInt);
+		
+		ball.setXSpeed((float) spInt / 1000);
+		ball.setYSpeed((float) spInt / 1000);
+		
+		
+		//// STARTING LOCATION
+		randNumYLoc = Math.random();
+		double y = (randNumYLoc * 60) + 1;
+		Gdx.app.log(TAG, "Location random " + y);
+		int yInt = (int)y;
+		if(yInt <=30){
+			yFloat = (float) (y * -1) / 100;
+			Gdx.app.log(TAG, "Location random y * -1 = " + yFloat );
+			ball.setYPosition(yFloat);
+			ball.setXSpeed(ball.getXSpeed() * -1);
+			ball.setYSpeed(ball.getYSpeed() * -1);
+		}else{
+			yFloat = (float) (y - 30) / 100;
+			Gdx.app.log(TAG, "Location random y - 30 / 100 = " + yFloat );
+			ball.setYPosition((yFloat - ball.getBallSprite().getWidth()));
+		}
+		
+		
+		Gdx.app.log(TAG, "yFloat " + yFloat );
+		Balls.add(ball);
+	}
+	
+	public void levelBallSet(int level){
+		for(int i = 1; i <= level; i++){
+			makeNewBall();
+		}
+	}
+	
 	@Override
 	public void dispose() {
 		batch.dispose();
 		bgTx.dispose();
-		grnBallTx.dispose();
-		grnBallTx2.dispose();
 		kidTx.dispose();
+		if(Balls.size > 0){
+			for(Ball ball: Balls) {
+				ball.ballTx.dispose();
+			};
+		}
 	}
 
 	@Override
 	public void render() {	
+			//Gdx.app.log(TAG, "#of balls" + Balls.size);
 		time += Gdx.graphics.getDeltaTime();   
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
@@ -170,143 +217,97 @@ public class MainKeepUp implements ApplicationListener, InputProcessor {
 		batch.begin();
 			bg.draw(batch);
 			kid.draw(batch);
-			ball.draw(batch);
-			ball2.draw(batch);
+			if(Balls.size > 0){
+				for(Ball ball: Balls) {
+					ball.draw(batch);
+				};
+			}
+			pause.draw(batch);
+		      
 			//font.draw(batch, "HELLO", 0, 0);  /// GRRRR  DOES NOT WORK
 		batch.end();
-			
-		ball1CheckAndSet();
-		ball2CheckAndSet();
-		Gdx.app.log(TAG, "TIME:" + time);
-		//float moveX = Interpolation.linear.apply(percent, game.manager.getProgress(), 0.1f);
 		
 		
+		if(Balls.size > 0){
+			for(Ball ball: Balls) {
+				ballLoopCheckAndSet(ball);
+			};
+		}
+		
+		//Gdx.app.log(TAG, "TIME:" + time);   //LOG LOG LOG LOG
 	}// END RENDER
 	
-	public void ball1CheckAndSet(){
-		float by = ball.getY() + YmoveSpeed;
-		float bx = ball.getX() + moveSpeed;
-		float bRotate = ball.getRotation() + rotateSpeed;
-		
-		if(bx + ball.getHeight() >= 0.5f){
-			moveSpeed = moveSpeed* -1;
-			if(SpinPos != false){
-				rotateSpeed = rotateSpeed * -1;
-			}
-			SpinPos = true;
-			bx = ball.getX() + moveSpeed;
-			bounce.play();
-		}
-		
-		if(by + ball.getWidth() >= .3f){
-			YmoveSpeed = YmoveSpeed* -1;
-			if(SpinPos != true){
-				rotateSpeed = rotateSpeed * -1;
-			}
-			SpinPos = false;
-			by = ball.getY() + YmoveSpeed;
-			bounce.play();	
-		}
-		
-		if(bx <= -0.5f){
-			moveSpeed = moveSpeed* -1;
-			if(SpinPos != false){
-				rotateSpeed = rotateSpeed * -1;
-			}
-			SpinPos = true;
-			bx = ball.getX() + moveSpeed;
-			bounce.play();
-		}
-		
-		if(by <= -.3f){
-			if(SpinPos != true){
-				rotateSpeed = rotateSpeed * -1;
-			}
-			SpinPos = false;
-			YmoveSpeed = YmoveSpeed* -1;
-			by = ball.getY() + YmoveSpeed;
-			bounce.play();	
-		}
-		
-		float moveX = Interpolation.linear.apply(ball.getX(), ball.getX() + moveSpeed, 1);
-		float moveY = Interpolation.linear.apply(ball.getY(), ball.getY() + YmoveSpeed, 1);
-		ball.setY(moveY);
-		ball.setX(moveX);
-		ball.setRotation(bRotate);
-		
-		boolean kidVSBallOverlap = ball.getBoundingRectangle().overlaps(kid.getBoundingRectangle());
-		if(kidVSBallOverlap == true){
-			buzzer.play(.05f);
-		}
-			
-	}
-	
-	public void ball2CheckAndSet(){
-		float by = ball2.getY() + YmoveSpeed2;
-		float bx = ball2.getX() + moveSpeed2;
-		float bRotate = ball2.getRotation() + rotateSpeed2;
-		
-		if(bx + ball2.getHeight() >= 0.5f){
-			moveSpeed2 = moveSpeed2* -1;
-			if(SpinPos2 != false){
-				rotateSpeed2 = rotateSpeed2 * -1;
-			}
-			SpinPos2 = true;
-			bx = ball2.getX() + moveSpeed2;
-			bounce.play();
-			
-		}
-		
-		if(by + ball2.getWidth() >= .3f){
-			YmoveSpeed2 = YmoveSpeed2* -1;
-			if(SpinPos2 != true){
-				rotateSpeed2 = rotateSpeed2 * -1;
-			}
-			SpinPos2 = false;
-			by = ball2.getY() + YmoveSpeed2;
-			bounce.play();
-			
-		}
-		
-		if(bx <= -0.5f){
-			moveSpeed2 = moveSpeed2* -1;
-			if(SpinPos2 != false){
-				rotateSpeed2 = rotateSpeed2 * -1;
-			}
-			SpinPos2 = true;
-			bx = ball2.getX() + moveSpeed2;
-			bounce.play();
-		}
-		
-		if(by <= -.3f){
-			if(SpinPos2 != true){
-				rotateSpeed2 = rotateSpeed2 * -1;
-			}
-			SpinPos2 = false;
-			
-			YmoveSpeed2 = YmoveSpeed2* -1;
-			by = ball2.getY() + YmoveSpeed2;
-			bounce.play();
-			
-		}
-		
-		float moveX = Interpolation.linear.apply(ball2.getX(), ball2.getX() + moveSpeed2, 1);
-		float moveY = Interpolation.linear.apply(ball2.getY(), ball2.getY() + YmoveSpeed2, 1);
-		ball2.setY(moveY);
-		ball2.setX(moveX);
-		ball2.setRotation(bRotate);
-		
-		boolean kidVSBall2Overlap = ball2.getBoundingRectangle().overlaps(kid.getBoundingRectangle());
-		if(kidVSBall2Overlap == true){
-			buzzer.play(.05f);
-			//kid.getScaleX();
-			//kid.getScaleY();
-			//Gdx.app.log(TAG, "SCALE X:" + kid.getHeight() + " Y:" + kid.getScaleY());
-			
-			//kid.setSize(kid.getHeight() + 0.01f, kid.getWidth() + 0.01f);  to much to long
-			//kid.rotate(90);
+	public void ballLoopCheckAndSet(Ball ball){
+		Sprite ballSprite = ball.getBallSprite();
 
+		float xPosition = ball.getXPosition();
+		float yPosition = ball.getYPosition();
+		int rotationSpeed = ball.getRotationSpeed();
+		boolean rotationDirection = ball.getRotation();
+		boolean collision = ball.getCollision();
+		float yPosSpriteHeight = yPosition + ballSprite.getWidth();
+		
+		
+		if(xPosition + ballSprite.getHeight() >= 0.5f){
+			//Gdx.app.log(TAG, "Out of bounds Down");
+			ball.setXSpeed(ball.getXSpeed() * -1);
+			ball.setXPosition(ball.getXPosition() + ball.getXSpeed());
+			if(rotationDirection != false){
+				ball.setRotationSpeed(rotationSpeed * -1);
+			}
+			ball.setRotation(true);
+			bounce.play();
+		}
+		
+		if(yPosSpriteHeight >= screenRatio / 2){
+			//Gdx.app.log(TAG, "Out of bounds Right");
+			ball.setYSpeed(ball.getYSpeed() * -1);
+			ball.setYPosition(ball.getYPosition() + ball.getYSpeed());
+			if(rotationDirection != true){
+				ball.setRotationSpeed(rotationSpeed * -1);
+			}
+			ball.setRotation(false);
+			bounce.play();	
+		}
+		if(xPosition <= -0.5f){
+			//Gdx.app.log(TAG, "Out of bounds Up");
+			ball.setXSpeed(ball.getXSpeed() * -1);
+			ball.setXPosition(ball.getXPosition() + ball.getXSpeed());
+			if(rotationDirection != false){
+				ball.setRotationSpeed(rotationSpeed * -1);
+			}
+			ball.setRotation(true);
+			bounce.play();
+		} 
+		if(yPosition <= (screenRatio / 2) * -1){
+			//Gdx.app.log(TAG, "Out of bounds Left");
+			ball.setYSpeed(ball.getYSpeed() * -1);
+			ball.setYPosition(ball.getYPosition() + ball.getYSpeed());
+			if(rotationDirection != true){
+				ball.setRotationSpeed(rotationSpeed * -1);
+			}
+			ball.setRotation(false);
+			bounce.play();	
+		}
 			
+		ball.setXPosition(ball.getXPosition() + ball.getXSpeed());
+		ball.setYPosition(ball.getYPosition() + ball.getYSpeed());
+		float bRotate = ballSprite.getRotation() + rotationSpeed;
+			
+			//float moveX = Interpolation.linear.apply(xPosition, xPosition + xSpeed, 1);
+			//float moveY = Interpolation.linear.apply(yPosition, yPosition + ySpeed, 1);
+		ballSprite.setX(ball.getXPosition() );
+		ballSprite.setY(ball.getYPosition());
+		ballSprite.setRotation(bRotate);
+		
+		boolean kidVSBallOverlap = ballSprite.getBoundingRectangle().overlaps(MainKeepUp.kid.getBoundingRectangle());
+		if(kidVSBallOverlap == true){
+			if(collision == false){
+				buzzer.play(.05f);
+				collision = true;
+			}
+		}else{
+			collision = false;
 		}
 			
 	}
@@ -410,70 +411,12 @@ public class MainKeepUp implements ApplicationListener, InputProcessor {
 		//float moveSpeedTime = 1f;
 		// get time of request
 		//float requestedAtTime = startTime;
-		
-		//qo46789y1v5pb98yt34oy8ces ;imcx aliuyw oefjyghbls.djvnaD
-		//DFLQIUGHPQWE;KFJH;QASLIDHJFKQ'AD';C"
+	
 		//Tween.to(man, tweenType, duration)
 		
 	};
 
 }
 
-class man {
-    private float x, y;
-    
-    public float getX() {
-        return x;
-    }
-    
-    public float getY() {
-        return y;
-    }
 
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-}
-
-///// FAILED  //////////
-//class manAccessor implements TweenAccessor<man> {
-//
-//    // The following lines define the different possible tween types.
-//    // It's up to you to define what you need :-)
-//
-//    public static final int POSITION_X = 1;
-//    public static final int POSITION_Y = 2;
-//    public static final int POSITION_XY = 3;
-//
-//
-//	@Override
-//	public int getValues(man target, int tweenType, float[] returnValues) {
-//		 switch (tweenType) {
-//         case POSITION_X: returnValues[0] = target.getX(); return 1;
-//         case POSITION_Y: returnValues[0] = target.getY(); return 1;
-//         case POSITION_XY:
-//             returnValues[0] = target.getX();
-//             returnValues[1] = target.getY();
-//             return 2;
-//         default: assert false; return -1;
-//     }
-//	}
-//
-//	@Override
-//	public void setValues(man target, int tweenType, float[] newValues) {
-//		 switch (tweenType) {
-//         case POSITION_X: target.setX(newValues[0]); break;
-//         case POSITION_Y: target.setY(newValues[0]); break;
-//         case POSITION_XY:
-//             target.setX(newValues[0]);
-//             target.setY(newValues[1]);
-//             break;
-//         default: assert false; break;
-//     }
-//	}
-//}
 
